a97b6cf63e8995b737ee05c6a6a8c1981e574457
pekadacreativestudio-hue <pekadacreativestudio@gmail.com>
2026-08-24 20:40:32 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/WAR/',
       server: {
         port: 3000,
         host: '0.0.0.0',
